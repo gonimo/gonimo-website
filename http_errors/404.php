c9556fbbf605c3ad6074b920662cd9f4c3cdb008
Georg Pichler <georg.pichler@gmail.com>
2016-11-11 13:56:35 +0100

    404 error corrections

--- http_errors/404.php
+++ http_errors/404.php
@@ -1,7 +1,8 @@
 <?php
+header('HTTP/1.0 404 Not Found')
 include '../header.php';
 ?>
-<title>Gonimo Impressum</title>
+<title>Gonimo - 404 Not Found</title>
 <div class="container-fluid s-i lvl-0">
 	<div class="container s-i lvl-1">
 	<h1> 404 Not Found </h1>
