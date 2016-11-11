140540ce83f5243500bedb2680ad0d6474faece1
Georg Pichler <georg.pichler@gmail.com>
2016-11-11 11:10:00 +0100

    Added 404 error page for testing

---
+++ http_errors/404.php
@@ -0,0 +1,17 @@
+<?php
+include '../header.php';
+?>
+<title>Gonimo Impressum</title>
+<div class="container-fluid s-i lvl-0">
+	<div class="container s-i lvl-1">
+	<h1> 404 Not Found </h1>
+	<p>
+	<img src="/img/err_404.jpg" />
+	</p
+	</div>
+</div>
+<?php 
+include '../footer.php';
+?>
+</body>
+</html>
