0fa13dd9a7c1e8d6ba3214a32821be4ff8ec5d44
Robert Klotzner <robert.klotzner@gmx.at>
2017-03-11 14:29:41 +0100

    Another try

--- index.php
+++ index.php
@@ -32,8 +32,8 @@ require_once 'header.php';
 			source += "md.png";
 		}else if(width > 768 && width <= 1024 ){
 			source += "lg.png";
-		}else { // We don't have an xl version:
-			source += "lg.png";
+		}else {
+			source += "xl.png";
 		}
 		$("#img-impact").attr("src",source);
 	});
