039d2d7e2ffccb18f6c4d874e0cf3e4e368387f0
Arnubil <ch.klotzner@gmail.com>
2017-04-19 21:32:29 +0200

    Update de.php
    "am besten" und "und"

--- lang/de.php
+++ lang/de.php
@@ -16,7 +16,7 @@ $i_i = [ "Schütze das Wertvolle im Leben."
        , "JETZT STARTEN"
        , "ALPHA-VERSION 0.9"
        , "Gonimo das Babyphon – Immer mit dabei."
-       , "Gonimo ist ein Babyphon, für dessen Benutzung du einfach einen Web-Browser benötigst.<br>Derzeit funktioniert Gonimo am Besten mit Chrome. An der Kompatibilität mit Firefox, Microsoft Edge, Opera oder Safari wird fleißig gearbeitet.<br>Über 
+       , "Gonimo ist ein Babyphon, für dessen Benutzung du einfach einen Web-Browser benötigst.<br>Derzeit funktioniert Gonimo am besten mit Chrome. An der Kompatibilität mit Firefox, Microsoft Edge, Opera und Safari wird fleißig gearbeitet.<br>Über 
    Jetzt Starten' kannst du sofort loslegen, ohne Download und ohne Installation.<br>"
        ];
 
