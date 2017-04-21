e8067429ab34234b433ecda95cee73edb2dac3bf
Arnubil <ch.klotzner@gmail.com>
2017-04-21 23:34:36 +0200

    FAQ-rework
    Hoffe er ladet dieses mal nur die de.php rauf

--- lang/de.php
+++ lang/de.php
@@ -118,51 +118,36 @@ $t = [ "Team"
 $f = [ "F.A.Q"
      , "Frequently Asked Questions"
      , "Hi, ich bin Gonimo. Hier findest du die Antworten auf die am häufigsten gestellten Fragen."
-     , "Was bin ich? "
-     , "Ich bin ein kostenloser Babymonitor, der auf jedem Gerät verwendet werden kann, welches einen aktuellen Chrome Browser installiert hat."
-     , "Was unterscheidet mich von anderen Babymonitoren?"
-     , "Du kannst mich mit einer Vielzahl unterschiedlicher Geräte verwenden, weil ich über den Browser funktioniere und keine eigene App installiert werden muss. Ich bin kostenlos und biete alle Funktionen in vollem Umfang. Zusätzlich biete ich eine herausragende Sicherheit. Du und deine Daten sind damit bestmöglich geschützt."
-     , "Was brauchst du, um mich verwenden zu können?"
-     , "Du brauchst ein Gerät, das ein Mikrofon und/oder eine Kamera besitzt und einen aktuellen Chrome Browser installiert hat. Bei längerer Verwendung ist sicher zu stellen, dass alle Geräte mit ausreichend Strom versorgt sind."
-     , "Kostet die Benutzung etwas?"
-     , "Nein!!! Gonimo ist ein Open Source Projekt, auch die Nutzung ist kostenlos, da es meinen Schöpfern ein großes Anliegen ist, meine umfangreichen Funktionen so vielen Menschen wie möglich, so einfach wie möglich und kostenlos zugänglich zu machen. Mehr Informationen dazu, was genau Open Source bedeutet, findest du <a href='https://de.wikipedia.org/wiki/Open_Source' title='Wikipedia: Open Source' target='_blank'> &gt hier &lt </a>"
-     , "Kann ich nicht auch einfach Skype verwenden?"
-     , "Natürlich kannst du auch Skype als Babyphon verwenden ...</p><p>Aber Skype ist nun mal ein Telefonieprogramm: </p><ul><li> Dein Baby kann nicht abheben, das macht es ein bisschen unpraktisch. Insbesondere wenn die Verbindung abreißt und du dich neu verbinden musst.</li><li> Wenn du in einem Telefonat ein Standbild bekommst (schlechte Verbindung), wirst du das merken - wenn du ein schlafendes Kind anschaust, eher nicht. Deswegen warnt dich Gonimo wenn du ein Standbild siehst.</li><li> Wenn die Verbindung in einem Telefonat abreißt, wirst du es merken. Wenn du einem schlafenden Kind zuhörst, eher nicht. Deswegen alarmiert dich Gonimo wenn die Verbindung abreisst.</li><li> Ganz allgemein ist Gonimo auf die Benutzung als Babyphon optimiert, während Skype auf die Nutzung als Telefon optimiert ist. Skype ist"
-     , "Wie hoch ist die Strahlenbelastung während ich eingeschaltet bin?"
-     , "Wenn die Babystation einen Meter entfernt von deinem Kind liegt, ist die Belastung sehr viel kleiner (ca. hunderttausend mal) als für dich, wenn du mit deinem Handy am Ohr telefonierst. Somit auch nicht höher, als die Belastung, wenn Facebook und Co. pausenlos Informationen aus dem Internet abfragen, oder du über deine Bluetooth-Köpfhörer Musik hörst."
-     , "Wieso wird es eine zusätzliche kostenpflichtige App geben?"
-     , "Die geplante kostenpflichtige App wird über die bereits voll ausgereizten Funktionen der Webapplikation hinaus zahlreiche Funktionen bieten, die nur über eine installierte App umsetzbar sind, weil ein Browser nicht alles auf einem Gerät steuern darf bzw. kann."
-     , "Wie hoch ist der mobile Datenverbrauch von mir?"
-     , "Befinden sich beide Geräte in einem WLAN Netzwerk, so wird gar kein mobiles Datenvolumen benötigt. Wenn ich nur mit Audio benutzt werde, ist die Übertragunsrate sehr gering. Bei gleichzeitigem Audio- und Videostreaming kann (je nach Qualität der Verbindung) wesentlich mehr Datenvolumen anfallen. Empfehlenswert ist, dies ein wenig im Auge zu behalten. "
-     , "Wie sicher ist die Verbindung zwischen den Geräten?"
-     , "Um die höchstmögliche Sicherheit zu gewährleisten, wurden bei mir folgende Sicherheitskonzepte umgesetzt: ein Einladungssystem, in welchem die Einladung bestätigt werden muss und eine Peer-to-Peer Verschlüsselung, um das 'Mithören' Dritter zu verhindern."
-     , "Kannst du mich nur als Babymonitor verwenden?"
-     , "Grundsätzlich sind meine Funktionen für die Benutzung als Babymonitor vorgesehen, aber natürlich kannst du mich für andere Aktivitäten auch verwenden, bei denen Du Audio- und/oder Videostreams verwenden willst"
-     , "Wie finanziert sich Gonimo?"
-     , "Damit das das Startup Projekt überhaupt gestartet und umgesetzt werden konnte, gab und gibt es eine Förderung durch NetIdee.at. Eine native (installierbare) App ist geplant, die gegen einen überschaubaren Betrag erhältlich sein wird. Sie wird Funktionen abdecken, die über einen Browser technisch nicht umsetzbar sind. Auch spenden ist möglich und wäre eine große Hilfe. Damit kann die stetige Verbesserung und Weiterentwicklung gewährleistet und besser vorangetrieben werden."
-     , "Kann ich das Projekt unterstützen?"
-     , "Ja. Du kannst das Gonimo-Projekt durch eine Spende über den Donate-Button oder durch den Erwerb der kostenpflichtigen App und/oder durch deinen Like auf verschiedenen Social-Media-Plattformen unterstützen. Natürlich sind wir auch immer auf der Suche nach kreativen und motivierten Leuten, die das Projekt mit ihrem Ehrgeiz und ihrem Know-How tatkräftig unterstützen wollen."
-     , "Werden meine Daten gespeichert oder an Dritte weitergegeben?"
-     , "Es werden bei mir nur die Daten gespeichert, die notwendig sind, um eine die Funktion zu gewährleisten. Diese Daten werden strikt nicht an Dritte weitergegeben und können auf deinen konkreten Wunsch hin auch jederzeit von den Servern gelöscht werden (Formular). Audio- und/oder Videodaten werden nicht gespeichert. Aufgrund der Peer-to-Peer Verbindung kann darauf auch nicht zugegriffen werden."
-     , "Gibt es eine Mindestspezifikation, um mich verwenden zu können?"
-     , "Es kann jede Hardware bzw. jedes Gerät verwendet werden auf dem die folgenden Browser installiert sind oder installiert werden können: Chrome, Firefox, Microsoft Edge, Opera oder Safari."
-     , "Wer sind meine Schöpfer?"
-     , "Ein Team junger, motivierter Leute, deren Profile du <a href='/team.php' title='Gonimo Team'> &gt hier &lt </a> findest."
-     , "Wenn ich ein eigenes Projekt auf Basis des Gonimo-Codes programmieren möchte, darf ich dann den Quellcode von Gonimo einfach kopieren und verwenden?"
-     , "Das Gonimo-Projekt ist ein OpenSource Projekt und unterliegt den Richtlinien der <a href='https://de.wikipedia.org/wiki/GNU_Affero_General_Public_License' title='GNU LICENSE' target='_blank'>AGPL GNU Affero General Public License</a> und somit darf der  <a href='https://github.com/gonimo' title='Source Code' target='_blank'>Quellcode</a>  unter deren Richtlinien verwendet werden. "
      , "Solltest deine Frage hier nicht geklärt worden sein, sende mir eine Nachricht über <a href='/kontakt.php' title='Gonimo Kontakt'> &gt dieses Kontaktformular &lt </a>. Ich freue mich, wenn ich weiter helfen kann."
      ];
 
 $faq = [
-		["Kategorie1"
-			,["FrageA","AntwortA"]
-			,["FrageB","AntwortB"]
-		],
-		["Kategorie2"
-		,["FrageC","AntwortC"]
-		,["FrageD","AntwortD"]
-		,["FrageE","AntwortE"]
-		]
+		["Allgemeines"
+			,["Was bin ich?","Ich bin ein kostenloses Babyphon, der auf jedem Gerät verwendet werden kann, welches einen aktuellen Chrome Browser installiert hat."]
+			,["Was unterscheidet mich von anderen Babyphonen?","Du kannst mich mit einer Vielzahl unterschiedlicher Geräte verwenden, weil ich über den Browser funktioniere und keine eigene App installiert werden muss. Ich bin kostenlos und biete alle Funktionen in vollem Umfang. Zusätzlich biete ich eine herausragende Sicherheit. Du und deine Daten sind damit bestmöglich geschützt."]
+      ,["Was brauchst du, um mich verwenden zu können?","Du brauchst ein Gerät, das ein Mikrofon und/oder eine Kamera besitzt und einen aktuellen Chrome Browser installiert hat. Bei längerer Verwendung ist sicher zu stellen, dass alle Geräte mit ausreichend Strom versorgt sind."]
+      ,["Kostet die Benutzung etwas?","Nein!!! Gonimo ist ein Open Source Projekt, auch die Nutzung ist kostenlos, da es meinen Schöpfern ein großes Anliegen ist, meine umfangreichen Funktionen so vielen Menschen wie möglich, so einfach wie möglich und kostenlos zugänglich zu machen. Mehr Informationen dazu, was genau Open Source bedeutet, findest du <a href='https://de.wikipedia.org/wiki/Open_Source' title='Wikipedia: Open Source' target='_blank'> &gt hier &lt </a>"]
+      ,["Kannst du mich nur als Babyphon verwenden?","Grundsätzlich sind meine Funktionen für die Benutzung als Babymonitor vorgesehen, aber natürlich kannst du mich für andere Aktivitäten auch verwenden, bei denen Du Audio- und/oder Videostreams verwenden willst"]
+      ,["Kann ich nicht auch einfach Skype verwenden?","Natürlich kannst du auch Skype als Babyphon verwenden ...</p><p>Aber Skype ist nun mal ein Telefonieprogramm: </p><ul><li> Dein Baby kann nicht abheben, das macht es ein bisschen unpraktisch. Insbesondere wenn die Verbindung abreißt und du dich neu verbinden musst.</li><li> Wenn du in einem Telefonat ein Standbild bekommst (schlechte Verbindung), wirst du das merken - wenn du ein schlafendes Kind anschaust, eher nicht. Deswegen warnt dich Gonimo wenn du ein Standbild siehst.</li><li> Wenn die Verbindung in einem Telefonat abreißt, wirst du es merken. Wenn du einem schlafenden Kind zuhörst, eher nicht. Deswegen alarmiert dich Gonimo wenn die Verbindung abreisst.</li><li> Ganz allgemein ist Gonimo auf die Benutzung als Babyphon optimiert, während Skype auf die Nutzung als Telefon optimiert ist."]
+      ,["Wie finanziert sich Gonimo?","Damit das das Startup Projekt überhaupt gestartet und umgesetzt werden konnte, gab und gibt es eine Förderung durch NetIdee.at. Eine native (installierbare) App ist geplant, die gegen einen überschaubaren Betrag erhältlich sein wird. Sie wird Funktionen abdecken, die über einen Browser technisch nicht umsetzbar sind. Auch spenden ist möglich und wäre eine große Hilfe. Damit kann die stetige Verbesserung und Weiterentwicklung gewährleistet und besser vorangetrieben werden."]
+      ,["Kann ich das Projekt unterstützen?","Ja. Du kannst das Gonimo-Projekt durch eine Spende über den Donate-Button oder durch den Erwerb der kostenpflichtigen App (In Entwiklung) und/oder durch deinen Like auf verschiedenen Social-Media-Plattformen unterstützen. Natürlich sind wir auch immer auf der Suche nach kreativen und motivierten Leuten, die das Projekt mit ihrem Ehrgeiz und ihrem Know-How tatkräftig unterstützen wollen."]
+      ,["Wer sind meine Schöpfer?","Ein Team junger, motivierter Leute, deren Profile du <a href='/team.php' title='Gonimo Team'> &gt hier &lt </a> findest."]
+    ],
+		["Sicherheit"
+		  ,["Wie sicher ist die Verbindung zwischen den Geräten?","Um die höchstmögliche Sicherheit zu gewährleisten, wurden bei uns folgende Sicherheitskonzepte umgesetzt: ein Einladungssystem, in welchem die Einladung bestätigt werden muss und eine Peer-to-Peer Verschlüsselung, um das 'Mithören' Dritter zu verhindern."]
+		  ,["Wie stabil ist die Verbindung zwischen den Geräten?","Wir haben eine sehr hohe Ausfallsicherheit, jedoch kann es trotzdem zu unerwünschten ausfällen kommen. Unser eingebauter Alarm bei Verbindungsproblemen, als auch die Musikkontrolle, sorgen für die entsprechende Sicherheit, damit du sofort weißt wenn es zu einem Verbindungsproblem gekommen ist."]
+		  ,["Werden meine Daten gespeichert oder an Dritte weitergegeben?","Es werden bei mir nur die Daten gespeichert, die notwendig sind, um eine die Funktion zu gewährleisten. Diese Daten werden strikt nicht an Dritte weitergegeben und können auf deinen konkreten Wunsch hin auch jederzeit von den Servern gelöscht werden (Formular). Audio- und/oder Videodaten werden nicht gespeichert. Aufgrund der Peer-to-Peer Verbindung kann darauf auch nicht zugegriffen werden."]
+    ],
+    ["Technisch"
+      ,["Wie hoch ist der mobile Datenverbrauch von mir?","Befinden sich beide Geräte in einem WLAN Netzwerk, so wird gar kein mobiles Datenvolumen benötigt. Wenn ich nur mit Audio benutzt werde, ist die Übertragunsrate sehr gering. Bei gleichzeitigem Audio- und Videostreaming kann (je nach Qualität der Verbindung) wesentlich mehr Datenvolumen anfallen. Empfehlenswert ist, dies ein wenig im Auge zu behalten."]
+      ,["Wie hoch ist die Strahlenbelastung während ich eingeschaltet bin?","Wenn das Babyphon einen Meter entfernt von deinem Kind liegt, ist die Belastung sehr viel kleiner (ca. hunderttausend mal) als für dich, wenn du mit deinem Handy am Ohr telefonierst. Somit auch nicht höher, als die Belastung, wenn Facebook und Co. pausenlos Informationen aus dem Internet abfragen, oder du über deine Bluetooth-Köpfhörer Musik hörst."]
+      ,["Wenn ich ein eigenes Projekt auf Basis des Gonimo-Codes programmieren möchte, darf ich dann den Quellcode von Gonimo einfach kopieren und verwenden?","Das Gonimo-Projekt ist ein OpenSource Projekt und unterliegt den Richtlinien der <a href='https://de.wikipedia.org/wiki/GNU_Affero_General_Public_License' title='GNU LICENSE' target='_blank'>AGPL GNU Affero General Public License</a> und somit darf der  <a href='https://github.com/gonimo' title='Source Code' target='_blank'>Quellcode</a>  unter deren Richtlinien verwendet werden."]
+    ],
+    ["App im PlayStore"
+      ,["Wieso wird es eine zusätzliche App geben?","Die geplante kostenpflichtige App wird über die bereits voll ausgereizten Funktionen der Webapplikation hinaus zahlreiche Funktionen bieten, die nur über eine installierte App umsetzbar sind, weil ein Browser nicht alles auf einem Gerät steuern darf bzw. kann."]
+      ,["Warum wird diese App etwas kosten?","Gonimo ist ein vollwertiges Babyphon, welches wir jedem zur Verfügung stellen wollen. Über die App kann nicht nur jeder Benutzer Gonimo unterstützen, sondern man erhält auch Zugriff auf Spezialfunktionen."]
+      ,["Wann wird es diese App geben?","Sie ist leider noch in Entwicklung, jedoch sind wir bemüht, dass wir sie so bald wie uns möglich veröffentlichen"]
+    ]
 ];
 
 $i = [ "Impressum"
