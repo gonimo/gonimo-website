114913cf840bb875b1a7fcb1f7d5e896c7b59f67
Robert Klotzner <robert.klotzner@gmx.at>
2017-05-02 18:48:38 +0200

    Fixed missing comma

--- lang/de.php
+++ lang/de.php
@@ -16,7 +16,7 @@ $i_i = [ "Schütze das <br> Wertvolle im Leben."
        , "JETZT STARTEN"
        , "ALPHA-VERSION 0.9"
        , "Gonimo das Babyphon – Immer mit dabei."
-       , "Gonimo ist ein Babyphon, für dessen Benutzung du einfach einen Web-Browser benötigst.<br>Derzeit funktioniert Gonimo am besten mit Chrome. <br> Firefox funktioniert in einer aktuellen Version, aber auch, Opera sollte ebenso funktionieren. An der Kompatibilität mit Microsoft Edge und Safari wird fleißig gearbeitet.<br>Über
+       , "Gonimo ist ein Babyphon, für dessen Benutzung du einfach einen Web-Browser benötigst.<br>Derzeit funktioniert Gonimo am besten mit Chrome. <br> Firefox funktioniert, in einer aktuellen Version, aber auch, Opera sollte ebenso funktionieren. An der Kompatibilität mit Microsoft Edge und Safari wird fleißig gearbeitet.<br>Über
    Jetzt Starten' kannst du sofort loslegen, ohne Download und ohne Installation.<br>"
        ];
 
